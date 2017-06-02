e0bd3f738b91566dd081dbc5592f5f0aaf925890
Artur Vaz <vaz985@gmail.com>
2017-06-02 13:54:26 -0300

    Resumindo classe rei
    Adicionado updatePos e evaluate

--- piece/Rei.java
+++ piece/Rei.java
@@ -4,10 +4,6 @@ import java.awt.Point;
 import javax.swing.JLabel;
 
 public class Rei extends Piece{
-	
-	public enum Direction{
-		Nordeste, Sudeste, Noroeste, Sudoeste
-	}
 
 	public Rei(int x, int y, int time){
 		super(x, y, time);
@@ -18,11 +14,43 @@ public class Rei extends Piece{
 	}
 
 	public void updatePosition(int x, int y){
-
+    validMoves.clearAll();
+    // Sul
+    if( x + 1 < 8 ) {
+      evaluatePosition(x + 1, y);
+    }
+    // Norte
+    if( x - 1 >= 0 ) {
+      evaluatePosition(x - 1, y);
+    }
+    // Direita
+    if( y + 1 < 8 ) {
+      evaluatePosition(x, y + 1);
+    }
+    // Esquerda
+    if( y - 1 >= 0 ) {
+      evaluatePosition(x, y - 1);
+    }
+    // Noroeste
+    if( x - 1 >= 0 && y - 1 >= 0 )  {
+      evaluatePosition(x - 1, y - 1);
+    }
+    // Nordeste
+    if( x - 1 >= 0 && y + 1 < 8 )  {
+      evaluatePosition(x - 1, y + 1);
+    }
+    // Sudoeste
+    if( x + 1 >= 0 && y - 1 >= 0 )  {
+      evaluatePosition(x + 1, y - 1);
+    }
+    // Sudeste
+    if( x + 1 >= 0 && y + 1 < 8 )  {
+      evaluatePosition(x + 1, y + 1);
+    }
 	//todo
 	}
 
-	public void evaluatePosition(int x, int y, Direction direction){
-		//todo	
+	public void evaluatePosition(int x, int y){
+    validMoves.add( new Point(x,y) );
 	}
 }
