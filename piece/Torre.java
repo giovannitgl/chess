a518d62a95a2cd2a2b1a4637e385ab86ea27510f
Artur Vaz <vaz985@gmail.com>
2017-06-04 14:31:10 -0300

    Funcionando!

--- piece/Torre.java
+++ piece/Torre.java
@@ -4,7 +4,7 @@ import java.awt.Point;
 import javax.swing.JLabel;
 
 public class Torre extends Piece{
-	
+
 	public enum Direction{
 		Norte, Sul, Esquerda, Direita
 	}
@@ -18,6 +18,64 @@ public class Torre extends Piece{
 	}
 
 	public void updatePosition(ArrayList<Piece> friend, ArrayList<Piece> enemy){
-		return;
+    validMoves.clear();
+    Point start = getLocation();
+    Point aux;
+    //Norte -> Sul
+    for (int i = (int)start.getX() + 1; i < 8; i++) {
+      aux = new Point( i, (int)start.getY() );
+      if( contains(friend, aux) ) {
+        break;
+      }
+      else if( contains(enemy, aux) ) {
+        validMoves.add(aux);
+        break;
+      }
+      else {
+        validMoves.add(aux);
+      }
+    }
+    //Sul -> Norte
+    for (int i = (int)start.getX() - 1; i >= 0; i--) {
+      aux = new Point( i, (int)start.getY() );
+      if( contains(friend, aux) ) {
+        break;
+      }
+      else if( contains(enemy, aux) ) {
+        validMoves.add(aux);
+        break;
+      }
+      else {
+        validMoves.add(aux);
+      }
+    }
+    //Oeste -> Leste
+    for (int i = (int)start.getY() + 1; i < 8; i++) {
+      aux = new Point( (int)start.getX(), i );
+      if( contains(friend, aux) ) {
+        break;
+      }
+      else if( contains(enemy, aux) ) {
+        validMoves.add(aux);
+        break;
+      }
+      else {
+        validMoves.add(aux);
+      }
+    }
+    //Leste -> Oeste
+    for (int i = (int)start.getY() - 1; i >= 0; i--) {
+      aux = new Point( (int)start.getX(), i );
+      if( contains(friend, aux) ) {
+        break;
+      }
+      else if( contains(enemy, aux) ) {
+        validMoves.add(aux);
+        break;
+      }
+      else {
+        validMoves.add(aux);
+      }
+    }
 	}
 }
