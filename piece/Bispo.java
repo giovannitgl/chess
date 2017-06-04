39ddd4ed44548f0034254f5d8be712b138d1581a
Artur Vaz <vaz985@gmail.com>
2017-06-04 14:47:08 -0300

    Bispo funcionando tb

--- piece/Bispo.java
+++ piece/Bispo.java
@@ -4,7 +4,7 @@ import java.awt.Point;
 import javax.swing.JLabel;
 
 public class Bispo extends Piece{
-	
+
 	public enum Direction{
 		Nordeste, Sudeste, Noroeste, Sudoeste
 	}
@@ -17,8 +17,76 @@ public class Bispo extends Piece{
 		else{} //Time de cima
 	}
 
-	public void updatePosition(ArrayList<Piece> friend, ArrayList<Piece> enemy){
-		return;
-	}
+  public void updatePosition(ArrayList<Piece> friend, ArrayList<Piece> enemy){
+    validMoves.clear();
+    Point start = getLocation();
+    Point aux;
+    int x, y;
+    // Nordeste
+    x = (int)start.getX() - 1;
+    y = (int)start.getY() + 1;
+    for (;x >= 0 && y < 8; x--, y++) {
+      aux = new Point( x, y );
+      if( contains(friend, aux) ) {
+        break;
+      }
+      else if( contains(enemy, aux) ) {
+        validMoves.add(aux);
+        break;
+      }
+      else {
+        validMoves.add(aux);
+      }
+    }
+    // Noroeste
+    x = (int)start.getX() - 1;
+    y = (int)start.getY() - 1;
+    for (;x >= 0 && y >= 0; x--, y--) {
+      aux = new Point( x, y );
+      if( contains(friend, aux) ) {
+        break;
+      }
+      else if( contains(enemy, aux) ) {
+        validMoves.add(aux);
+        break;
+      }
+      else {
+        validMoves.add(aux);
+      }
+    }
+    // Sudeste
+    x = (int)start.getX() + 1;
+    y = (int)start.getY() + 1;
+    for (;x < 8 && y < 8; x++, y++) {
+      aux = new Point( x, y );
+      if( contains(friend, aux) ) {
+        break;
+      }
+      else if( contains(enemy, aux) ) {
+        validMoves.add(aux);
+        break;
+      }
+      else {
+        validMoves.add(aux);
+      }
+    }
+    // Sudoeste
+    x = (int)start.getX() + 1;
+    y = (int)start.getY() - 1;
+    for (;x < 8 && y >= 0; x++, y--) {
+      aux = new Point( x, y );
+      if( contains(friend, aux) ) {
+        break;
+      }
+      else if( contains(enemy, aux) ) {
+        validMoves.add(aux);
+        break;
+      }
+      else {
+        validMoves.add(aux);
+      }
+    }
+
+  }
 
 }
