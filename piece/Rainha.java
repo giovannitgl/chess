5d952eedca778f28ce5e2a11fe6bd0c14f405bfe
giovannitgl <giovannitgl@dcc.ufmg.br>
2017-06-04 15:25:28 -0300

    Rainha funcional

--- piece/Rainha.java
+++ piece/Rainha.java
@@ -22,52 +22,116 @@ public class Rainha extends Piece{
     int auxY = (int)p.getY();
   
   //esquerda
-    for(int i = auxX-1; i >= 0; i--){
-      aux.setLocation(i,auxY);
-      if(!this.contains(friend,aux)){
-        validMoves.add(new Point(i,auxY));
-      }
-      if(this.contains(enemy,aux)){
-        validMoves.add(new Point(i,auxY));
-        break;
-      }
-    }
-  //direita
-    for(int i = auxX+1; i < 8; i++){
-      aux.setLocation(i,auxY);
-      if(!this.contains(friend,aux)){
-        validMoves.add(new Point(i,auxY));
-      }
-      if(this.contains(enemy,aux)){
-        validMoves.add(new Point(i,auxY));
-        break;
-      }
-    }
-  //cima
-   for(int i = auxY+1; i < 8; i++){
-      aux.setLocation(auxX,i);
-      if(!this.contains(friend,aux)){
-        validMoves.add(new Point(auxX,i));
-      }
-      if(this.contains(enemy,aux)){
-        validMoves.add(new Point(auxX,i));
-        break;
-      }
-    }
-  //baixo
     for(int i = auxY-1; i >= 0; i--){
       aux.setLocation(auxX,i);
-      if(!this.contains(friend,aux)){
-        validMoves.add(new Point(auxX,i));
-      }
       if(this.contains(enemy,aux)){
         validMoves.add(new Point(auxX,i));
         break;
       }
+      else if(this.contains(friend,aux)){
+        break;
+      }
+      else
+        validMoves.add(new Point(auxX,i));
     }
-  //esquerda + cima
-  //esquerda + baixo
-  //direita + cima
-  //direita + baixo
+  //direita
+    for(int i = auxY+1; i < 8; i++){
+      aux.setLocation(auxX,i);
+      if(this.contains(enemy,aux)){
+        validMoves.add(new Point(auxX,i));
+        break;
+      }
+      else if(this.contains(friend,aux)){
+        break;
+      }
+      else
+        validMoves.add(new Point(auxX,i));
+    }
+  //cima
+     for(int i = auxX+1; i < 8; i++){
+      aux.setLocation(i,auxY);
+      if(this.contains(enemy,aux)){
+        validMoves.add(new Point(i,auxY));
+        break;
+      }
+      else if(this.contains(friend,aux)){
+        break;
+      }
+      else
+        validMoves.add(new Point(i,auxY));
+    }
+    //baixo
+      for(int i = auxX-1; i >= 0; i--){
+      aux.setLocation(i,auxY);
+      if(this.contains(enemy,aux)){
+        validMoves.add(new Point(i,auxY));
+        break;
+      }
+      else if(this.contains(friend,aux)){
+        break;
+      }
+      else
+        validMoves.add(new Point(i,auxY));
+    }
+    //esquerda + cima
+      int i = auxX + 1; // linha
+      int j = auxY - 1; // coluna
+      for(; i < 8 && j >= 0; i++, j--){
+      aux.setLocation(i,j);
+      if(this.contains(enemy,aux)){
+        validMoves.add(new Point(i,j));
+        break;
+      }
+      else if(this.contains(friend,aux)){
+        break;
+      }
+      else
+        validMoves.add(new Point(i,j));
+    }
+    //esquerda + baixo
+      i = auxX - 1; // linha
+      j = auxY - 1; // coluna
+    for(; i >= 0 && j >= 0; i--, j--){
+      aux.setLocation(i,j);
+      if(this.contains(enemy,aux)){
+        validMoves.add(new Point(i,j));
+        break;
+      }
+      else if(this.contains(friend,aux)){
+        break;
+      }
+      else
+        validMoves.add(new Point(i,j));
+    }
+    //direita + cima
+    i = auxX + 1; // linha
+    j = auxY + 1; // coluna
+    for(; i < 8 && j < 8; i++, j++){
+      aux.setLocation(i,j);
+      if(this.contains(enemy,aux)){
+        validMoves.add(new Point(i,j));
+        break;
+      }
+      else if(this.contains(friend,aux)){
+        break;
+      }
+      else
+        validMoves.add(new Point(i,j));
+    }
+     //direita + baixo
+    i = auxX - 1; // linha
+    j = auxY + 1; // coluna
+    for(; i >= 0 && j < 8; i--, j++){
+      aux.setLocation(i,j);
+      if(this.contains(enemy,aux)){
+        validMoves.add(new Point(i,j));
+        break;
+      }
+      else if(this.contains(friend,aux)){
+        break;
+      }
+      else
+        validMoves.add(new Point(i,j));
+    }
   }
 }
