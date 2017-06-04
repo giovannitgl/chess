4ef013f8c5a58fe8a0ca77987fdbbdd9fafe65fc
davibeltrao <davibeltrao02@gmail.com>
2017-06-04 01:47:06 -0300

    Jogadores Adicionados. Falta remover peças comidas e atualizar no View quando a peca é comida(remover a peça)

--- Tabuleiro.java
+++ Tabuleiro.java
@@ -3,18 +3,19 @@ import java.awt.Point;
 import java.util.ArrayList;
 
 public class Tabuleiro{
-	//Player p1
-	//Player p2
+	Jogador j0;
+	Jogador j1;
 	// Point
 	// protected Piece[][] tabuleiro;
 	protected Space[][] tabuleiro;
 	Tabuleiro(){
-		// tabuleiro = new Piece[8][8];
 		tabuleiro = new Space[8][8];
 		buildTabuleiro();
 	}
 
 	public void buildTabuleiro(){
+		j0 = new Jogador(0);
+		j1 = new Jogador(1);
 		buildWhite();
 		buildBlack();
 		buildMiddle();
@@ -24,48 +25,67 @@ public class Tabuleiro{
 		for(int i = 0; i < 8; i++){
 			// Peao p = new Peao(6,i,0);
 			tabuleiro[6][i] = new Space(new Peao(6,i,0));
+			j0.addPiece(tabuleiro[6][i].getPiece());
 		}
 		// Torre t1 = new Torre(7,0,0);
 		// Torre t2 = new Torre(7,7,0);
 		tabuleiro[7][0] = new Space(new Torre(7,0,0));
 		tabuleiro[7][7] = new Space(new Torre(7,7,0));
+		j0.addPiece(tabuleiro[7][0].getPiece());
+		j0.addPiece(tabuleiro[7][7].getPiece());
 
 		// Cavalo c1 = new Cavalo(7,1,0);
 		// Cavalo c2 = new Cavalo(7,6,0);
 		tabuleiro[7][1] = new Space(new Cavalo(7,1,0));
 		tabuleiro[7][6] = new Space(new Cavalo(7,6,0));
+		j0.addPiece(tabuleiro[7][1].getPiece());
+		j0.addPiece(tabuleiro[7][6].getPiece());
 
 		// Bispo b1 = new Bispo(7,2,0);
 		// Bispo b2 = new Bispo(7,5,0);
 		tabuleiro[7][2] = new Space(new Bispo(7,2,0));
 		tabuleiro[7][5] = new Space(new Bispo(7,5,0));
+		j0.addPiece(tabuleiro[7][2].getPiece());
+		j0.addPiece(tabuleiro[7][5].getPiece());
 
 		tabuleiro[7][3] = new Space(new Rainha(7,3,0));
 		tabuleiro[7][4] = new Space(new Rei(7,4,0));
+		j0.addPiece(tabuleiro[7][3].getPiece());
+		j0.addPiece(tabuleiro[7][4].getPiece());
 	}
 
 	private void buildBlack(){
 		for(int i = 0; i < 8; i++){
 			// Peao p = new Peao(1,i,1);
 			tabuleiro[1][i] = new Space(new Peao(1,i,1));
+			j1.addPiece(tabuleiro[1][i].getPiece());
 		}
 
 		// Torre t1 = new Torre(0,0,1);
 		// Torre t2 = new Torre(0,7,1);
 		tabuleiro[0][0] = new Space(new Torre(0,0,1));
 		tabuleiro[0][7] = new Space(new Torre(0,7,1));
+		j1.addPiece(tabuleiro[0][0].getPiece());
+		j1.addPiece(tabuleiro[0][7].getPiece());
 
 		// Cavalo c1 = new Cavalo(0,1,1);
 		// Cavalo c2 = new Cavalo(0,6,1);
 		tabuleiro[0][1] = new Space(new Cavalo(0,1,1));
 		tabuleiro[0][6] = new Space(new Cavalo(0,6,1));
+		j1.addPiece(tabuleiro[0][1].getPiece());
+		j1.addPiece(tabuleiro[0][6].getPiece());
 
 		// Bispo b1 = new Bispo(0,2,1);
 		// Bispo b2 = new Bispo(0,5,1);
 		tabuleiro[0][2] = new Space(new Bispo(0,2,1));
 		tabuleiro[0][5] = new Space(new Bispo(0,5,1));
+		j1.addPiece(tabuleiro[0][2].getPiece());
+		j1.addPiece(tabuleiro[0][5].getPiece());
+
 		tabuleiro[0][3] = new Space(new Rainha(0,3,1));
 		tabuleiro[0][4] = new Space(new Rei(0,4,1));
+		j1.addPiece(tabuleiro[0][3].getPiece());
+		j1.addPiece(tabuleiro[0][4].getPiece());
 	}
 	private void buildMiddle(){
 		for(int i = 2; i < 6; i ++){
