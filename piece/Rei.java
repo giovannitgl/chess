0e22ae32eeed744a14dd14e5c8e5ac33ca9a3967
Artur Vaz <vaz985@gmail.com>
2017-06-09 22:07:27 -0300

    Check do peao feito!

--- piece/Rei.java
+++ piece/Rei.java
@@ -13,7 +13,70 @@ public class Rei extends Piece{
 		else{} //Time de cima
 	}
 
-	 public void updatePosition(ArrayList<Piece> friend, ArrayList<Piece> enemy){
-    return;
+  public boolean checkPawn(Piece enemy, Point p) {
+    Point aux = enemy.getLocation();
+    if(aux.getX() + 1 == p.getX() && aux.getY() + 1 == p.getY()) {
+      return false;
+    }
+    else
+    if(aux.getX() + 1 == p.getX() && aux.getY() - 1 == p.getY()) {
+      return false;
+    }
+    else
+    if(aux.getX() - 1 == p.getX() && aux.getY() + 1 == p.getY()) {
+      return false;
+    }
+    else
+    if(aux.getX() - 1 == p.getX() && aux.getY() - 1 == p.getY()) {
+      return false;
+    }
+    return true;
+  }
+
+  public boolean
+  public boolean isSafe(ArrayList<Piece> enemy, Point p)  {
+    for (int i = 0; i < enemy.size() ; i++ ) {
+      Piece pc = enemy.get(i);
+      switch(pc.getType()) {
+        case PAWN:
+          if(checkPawn(pc, p) == false)  {return false;}
+        case KNIGHT:
+
+        case BISHOP:
+
+        case ROOK:
+
+        case QUEEN:
+
+        case KING:
+      }
+    }
+    return true;
+  }
+
+  public void updatePosition(ArrayList<Piece> friend, ArrayList<Piece> enemy){
+    validMoves.clear();
+    Point start = getLocation();
+    Point aux;
+    int x, y;
+    ArrayList<Point> temp;
+    // Norte -> Sul
+    x = (int)start.getX() + 1;
+    y = (int)start.getY();
+    aux = new Point(x,y);
+    if ( isSafe(enemy,aux) ) {
+      validMoves.add(aux);
+
+    }
+    // Sul -> Norte
+    x = (int)start.getX() - 1;
+    y = (int)start.getY();
+    aux = new Point(x,y);
+    if ( isSafe(enemy,aux) ) {
+      validMoves.add(aux);
+
+    }
+
+
   }
 }
