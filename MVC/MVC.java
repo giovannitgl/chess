f53eaaafa0b2f7cc5764489f6130755be672d8ab
Artur Vaz <vaz985@gmail.com>
2017-06-18 20:50:12 -0300

    MVC conferido

--- MVC/MVC.java
+++ MVC/MVC.java
@@ -1,6 +1,7 @@
 package MVC;
 import model.*;
 import gui.*;
+
 public class MVC {
   private Model m = Model.getInstance();
   private View  v;
@@ -11,9 +12,7 @@ public class MVC {
     c.setMode(0);
     v = new View(c);
     m.setView(v);
-    // m.buildTabuleiro();
     c.setModel(m);
-    // m = new Model(v);
   }
 
 }
